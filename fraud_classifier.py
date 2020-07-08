2beede333ad816e31ddcd248b60083ea05026c3c
Nish Shyam <nshyam668@gmail.com>
2020-07-08 08:56:56 +0100

    daily update jupyter

--- fraud_classifier.py
+++ fraud_classifier.py
@@ -7,11 +7,12 @@ from imblearn.under_sampling import RandomUnderSampler
 from imblearn.over_sampling import SMOTE
 from imblearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import f1_score, make_scorer, accuracy_score
+from sklearn.metrics import f1_score, make_scorer, confusion_matrix, accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 import tensorflow as tf
 from tensorflow import keras
+from keras.optimizers import SGD
 from plots import plot_3d
 
 data = pandas.read_csv('creditcard.csv')
@@ -19,8 +20,11 @@ data = pandas.read_csv('creditcard.csv')
 # Imbalanced dataset so will need to resample
 print(data['Class'].value_counts())
 
-X_train, X_test, y_train, y_test = train_test_split(data.drop('Class', axis=1), data.Class, test_size=0.25,
+X_train, X_test, y_train, y_test = train_test_split(data.drop('Class', axis=1), data.Class, test_size=0.20,
                                                     random_state=42)
+
+print(y_train.value_counts()[1] / len(y_train))
+print(y_test.value_counts()[1] / len(y_test))
 # Under Sampling to deal with imbalance
 rus = RandomUnderSampler(random_state=42, sampling_strategy='majority')
 X_usampled, y_usampled = rus.fit_sample(X_train, y_train)
@@ -38,15 +42,14 @@ sm = SMOTE(sampling_strategy='minority', random_state=7)
 def cross_validation(model, params, X_train, Y_train):
     f1_scorer = make_scorer(f1_score, pos_label=1)
     gs = GridSearchCV(model, params, cv=10, scoring=f1_scorer)
-    X_osampled, y_osampled = sm.fit_sample(X_train, Y_train)
-    gs.fit(X_osampled, y_osampled)
+    gs.fit(X_train, Y_train)
     print('F1 Score: ')
     print(gs.best_score_)
     print(gs.best_estimator_)
     return gs.best_estimator_
 
 
-# # Logistic Regression Under
+# Logistic Regression Under
 lR = LogisticRegression()
 parameters = [{}]
 start = datetime.now()
@@ -54,8 +57,8 @@ lRuEstimators = cross_validation(lR, parameters, X_usampled, y_usampled)
 lRufit = lRuEstimators.fit(X_usampled, y_usampled)
 print(lRufit.coef_)
 print('Logistic Regression, Under Sampled: ', datetime.now() - start)
-# #
-# # Random Forest Under
+#
+# Random Forest Under
 rF = RandomForestClassifier()
 parameters = [{'max_leaf_nodes': [8], 'max_depth': [6], 'min_samples_split': [8],
                'min_samples_leaf': [8]}]
@@ -64,7 +67,7 @@ rfuEstimators = cross_validation(rF, parameters, X_usampled, y_usampled)
 print(rfuEstimators.feature_importances_)
 rFufit = rfuEstimators.fit(X_usampled, y_usampled)
 print('Random Forest, Under Sampled: ', datetime.now() - start)
-# #
+#
 # Logistic Regression Over
 parameters = [{}]
 start = datetime.now()
@@ -84,29 +87,37 @@ print('Logistic Regression, Over Sampled: ', datetime.now() - start)
 # print('Random Forest, Over Sampled: ', datetime.now() - start)
 
 # # Sequential Deep Learning Model
-# model = keras.models.Sequential([
+# dl_model = keras.models.Sequential([
 #     keras.layers.InputLayer(input_shape=(30,)),
-#     keras.layers.Dense(100, activation='relu'),
 #     keras.layers.Dense(30, activation='relu'),
 #     keras.layers.Dense(1, activation='sigmoid')
 # ])
+# opt = SGD(learning_rate=0.001)
+# dl_model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
 #
-# model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
+# X_osampled, y_osampled = sm.fit_sample(X_train, y_train)
+# df = pandas.concat((X_osampled, y_osampled), axis=1, join='inner')
+# df = df.sample(frac=1).reset_index(drop=True)
+# X_osampled = df.drop('Class', axis=1)
+# y_osampled = df['Class']
 #
-# checkpoint = keras.callbacks.ModelCheckpoint('keras_model.h5')
+# checkpoint = keras.callbacks.ModelCheckpoint('keras_model.h5', save_best_only=True)
 # early_stopping = keras.callbacks.EarlyStopping(patience=5, restore_best_weights=True)
 #
-# X_test = pandas.DataFrame(scaler.fit_transform(X_test))
-# X_test = pandas.DataFrame(pca.fit_transform(X_test))
-
-# history = model.fit(X_osampled, y_osampled, epochs=30, validation_split=0.25, callbacks=[checkpoint, early_stopping])
-# y_pred_o = numpy.argmax(model.predict(X_test), axis=-1)
+# history = dl_model.fit(X_osampled, y_osampled, epochs=30, validation_split=0.2, shuffle=True,
+#                        callbacks=[early_stopping, checkpoint])
+# y_pred_o = dl_model.predict(X_test)
+# print(list(y_pred_o))
+# confusion_matrix(y_test, y_pred_o, labels=(1, 0))
 # over_nn = accuracy_score(y_test, y_pred_o)
-# history2 = model.fit(X_usampled, y_usampled, epochs=30, validation_split=0.1, callbacks=[checkpoint, early_stopping])
-# y_pred_u = numpy.argmax(model.predict(X_test), axis=-1)
+
+# history2 = dl_model.fit(X_usampled, y_usampled, epochs=30, validation_split=0.2, shuffle=True,
+#                         callbacks=[early_stopping, checkpoint])
+# y_pred_u = numpy.argmax(dl_model.predict(X_test), axis=-1)
 # under_nn = accuracy_score(y_test, y_pred_u)
 
 lRu_y = lRufit.predict(X_test)
+print(confusion_matrix(y_test, lRu_y))
 print(accuracy_score(y_test, lRu_y))
 rFu_y = rFufit.predict(X_test)
 print(accuracy_score(y_test, rFu_y))
@@ -116,8 +127,3 @@ print(accuracy_score(y_test, lRo_y))
 # print(accuracy_score(y_test, rFo_y))
 # print(over_nn)
 # print(under_nn)
-
-
-# Xu_train_pca.columns = ['pca_one', 'pca_two', 'pca_three']
-#
-# plot_3d(Xu_train_pca, y_usampled)
